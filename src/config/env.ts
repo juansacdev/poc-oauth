cb0d96e1b1f7ed03eec04df8a435550756187e69
Juan Sebastián Agudelo <juansac.me@gmail.com>
2022-08-15 23:26:41 -0500

    Set env config variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,12 +1,24 @@
 import 'dotenv/config'
 import path from 'path'
 
-const { NODE_ENV, PORT, URL_API } = process.env
+const {
+  NODE_ENV,
+  PORT,
+  URL_API,
+  GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET
+} = process.env
 
 export const config = {
   env: {
     isDev: NODE_ENV !== 'production',
-    port: Number(PORT)
+    port: Number(PORT),
+    githubClientId: GITHUB_CLIENT_ID ?? '',
+    githubClientSecret: GITHUB_CLIENT_SECRET ?? '',
+    googleClientId: GOOGLE_CLIENT_ID ?? '',
+    googleClientSecret: GOOGLE_CLIENT_SECRET ?? ''
   },
   constants: {
     BASE_URL_API: '/api/v1',
